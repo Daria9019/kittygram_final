6b311a15d7f600a1905b3f932b1d752c4dd73cb0
Daria9019 <dariik39@gmail.com>
2023-07-24 17:46:32 +0800

    Not work

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.164.26', 'localhost', 'kittygram90.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -92,9 +92,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/frontend_static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
