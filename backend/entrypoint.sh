b0c00f57afea714beca4f1a9aadbe38171a205da
Daria9019 <dariik39@gmail.com>
2023-07-30 17:39:53 +0800

    50

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-if [ -d "/app/collected_static" ]; then
-  cp -r /app/collected_static/. /backend_static/static/
-else
-  echo "Error: /app/collected_static not found."
-  exit 1
-fi
-
 python manage.py migrate
 python manage.py collectstatic --noinput
