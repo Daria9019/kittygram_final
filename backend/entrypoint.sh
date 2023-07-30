5c6af550fcb2a181e79d8a5d92ca54f7b8f40413
Daria9019 <dariik39@gmail.com>
2023-07-30 18:54:11 +0800

    100

--- backend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-python manage.py migrate
-python manage.py collectstatic --no-input
-cp -r /app/collected_static/. /backend_static/static/
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
