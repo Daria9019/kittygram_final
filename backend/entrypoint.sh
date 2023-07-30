bd0b5f15053a560387f6d0bde4a6083edb213a73
Daria9019 <dariik39@gmail.com>
2023-07-30 16:11:00 +0800

    21

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Выполнение миграций базы данных
 python manage.py migrate
 
+# Сбор статических файлов Django
 python manage.py collectstatic --noinput
 
+# Запуск Gunicorn для обслуживания приложения
 exec gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
